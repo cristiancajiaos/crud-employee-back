b33b185e72c74b0c1b4e4145405d9d218e21d268
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-24 14:27:13 -0400

    Primer commit

---
+++ src/main/java/com/example/crudemployeeback/CrudEmployeeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudemployeeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrudEmployeeBackApplication.class, args);
+  }
+
+}
